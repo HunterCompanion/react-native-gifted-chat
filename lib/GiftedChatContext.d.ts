e1f2c5389a0dac874f404373bd03346607789b18
Filip Frlić <filipfrlic@gmail.com>
2025-08-19 10:09:30 +0200

    Remove lib from repo and add to .gitignore

--- lib/GiftedChatContext.d.ts
+++
@@ -1,5 +0,0 @@
-export interface IGiftedChatContext {
-    getLocale(): string;
-}
-export declare const GiftedChatContext: import("react").Context<IGiftedChatContext>;
-export declare const useChatContext: () => IGiftedChatContext;
